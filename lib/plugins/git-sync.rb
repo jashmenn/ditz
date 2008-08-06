d5a8725b9405dfb4f93dc6f7c3c1ae8908cb87a6
William Morgan <wmorgan-ditz@masanjin.net>
2008-08-05 19:16:36 -0700

    minor bugfixes and tweaks to git-sync plugins

--- lib/plugins/git-sync.rb
+++ lib/plugins/git-sync.rb
@@ -1,19 +1,20 @@
 module Ditz
 class Operator
   operation :sync, "Sync the repo containing ditz via git"
-  def sync project, config, issue, maybe_string
+  def sync project, config
     Dir.chdir $project_root
-    sh "git commit -m 'issue updates'"
+    puts "[in #{$project_root}]"
+    sh "git commit -a -m 'issue updates'", :force => true
     sh "git pull"
     sh "git push"
   end
 
   private
 
-  def sh s
+  def sh s, opts={}
     puts "+ #{s}"
-    unless system s
-      $stderr.puts "can't execute: #{s}"
+    unless opts[:force] || system(s)
+      $stderr.puts "non-zero (#{$?.exitstatus}) exit code: #{s}"
       exit(-1)
     end
   end
