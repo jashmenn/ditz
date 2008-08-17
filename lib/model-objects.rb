ea39c3fafa28ab59f566c9a06f460fbec850c547
William Morgan <wmorgan-ditz@masanjin.net>
2008-08-17 12:28:45 -0700

    make config object prompt for issue dir
    Let this be a configurable variable. Also important because ditz reconfigure
    only preserves promptable fields right now.

--- lib/model-objects.rb
+++ lib/model-objects.rb
@@ -314,7 +314,7 @@ end
 class Config < ModelObject
   field :name, :prompt => "Your name", :default_generator => :get_default_name
   field :email, :prompt => "Your email address", :default_generator => :get_default_email
-  field :issue_dir, :ask => false, :default => "bugs"
+  field :issue_dir, :prompt => "Directory to store issues state in", :default => "bugs"
 
   def user; "#{name} <#{email}>" end
 
